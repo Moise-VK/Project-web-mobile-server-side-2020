08c334b8b1b94515d3efaffe6bbf2d03369596ff
Moïse VanKeymeulen <moise.vankeymeulen@student.odisee.be>
2020-11-09 22:53:41 +0100

    form validation

--- app/public/contact.php
+++ app/public/contact.php
@@ -10,8 +10,62 @@
 
 
     // Data
+    $name = '';
+    $email = '';
+    $subject = '';
+    $message = '';
 
+    $errName = false;
+    $errEmail = false;
+    $errSubject = false;
+    $errMessage = false;
+
+    $checkFilled = 0;
+
+
+    if (isset($_GET['name']) && $_GET['name'] != ''){
+        $name = $_GET['name'];
+        $checkFilled++;
+    }
+    elseif (isset($_GET['name']) && $_GET['name'] == ''){
+        $errName = true;
+    }
+
+    if (isset($_GET['email']) && $_GET['email'] != ''){
+        $email = $_GET['email'];
+        $checkFilled++;
+    }
+    elseif (isset($_GET['email']) && $_GET['email'] == ''){
+        $errEmail = true;
+    }
+
+    if (isset($_GET['subject']) && $_GET['subject'] != ''){
+        $subject = $_GET['subject'];
+        $checkFilled++;
+    }
+    elseif (isset($_GET['subject']) && $_GET['subject'] == ''){
+        $errSubject = true;
+    }
+
+    if (isset($_GET['message']) && $_GET['message'] != ''){
+        $message = $_GET['message'];
+        $checkFilled++;
+    }
+    elseif (isset($_GET['message']) && $_GET['message'] == ''){
+        $errMessage = true;
+    }
 
     // View
     $tpl = $twig->load('/pages/contact.twig');
-    echo $tpl->render();
+    echo $tpl->render([
+        'name' => $name,
+        'email' => $email,
+        'subject' => $subject,
+        'message' => $message,
+        'errName' => $errName,
+        'errEmail' => $errEmail,
+        'errSubject' => $errSubject,
+        'errMessage' => $errMessage,
+        'filled' => $checkFilled
+    ]);
+
