d60f829253d9d9e16fd62bc68de02c2e91d359b9
Jonathan De Mangelaere <jonathan@jon-it.be>
2020-12-06 17:51:56 +0100

    Login/register works again

--- app/src/Http/Requests/AuthController.php
+++ app/src/Http/Requests/AuthController.php
@@ -22,29 +22,24 @@
             ]);
         }
 
-        private function checkIfUserExists(string $email) : bool{
-            $stmt = $this->db->prepare('SELECT * FROM users WHERE email = ?');
-            $stmt->execute([$email]);
-            $userData = $stmt->fetchAssociative();
-
-            if(count($userData) > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
         public function login() {
             $email = isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? trim($_POST['email']) : '';
             $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+
             if (isset($_POST['moduleAction']) && ($_POST['moduleAction'] == 'login')) {
-                if(count($userData) > 0 && password_verify($password, $userData['password']) == true){
+
+                $stmt = $this->db->prepare('SELECT * FROM users WHERE email = ?');
+                $stmt->execute([$email]);
+                $userData = $stmt->fetchAllAssociative();
+
+                if(count($userData) > 0 && password_verify($password, $userData[0]['password']) == true){
                     $_SESSION['user'] = $email;
 
                     if (isset($_POST['remember'])) {
                         setcookie('email', $email, time() + 60 * 60 * 24 * 30);
                     }
                     $this->returnToOverview('home');
+
                 } else {
                     $this->returnToOverview('login?error=true&email=' . $email);
                 }
@@ -66,15 +61,20 @@
             $repeatPassword = isset($_POST['repeatPassword']) ? trim($_POST['repeatPassword']) : '';
 
             if($email && $password && $this->equalPasswords($password, $repeatPassword) == true && isset($_POST['moduleAction']) && ($_POST['moduleAction'] == 'register')){
-                if(!$this->checkIfUserExists($email)){
+                $stmt = $this->db->prepare('SELECT * FROM users WHERE email = ?');
+                $stmt->execute([$email]);
+                $userData = $stmt->fetchAllAssociative();
+
+                if(count($userData) == 0){
+                    $data = [$email, password_hash($password, PASSWORD_BCRYPT)];
                     $stmt = $this->db->prepare('INSERT INTO users(email, password) VALUES(?,?)');
-                    $stmt->execute([$email, password_hash($password, PASSWORD_DEFAULT)]);
+                    $stmt->execute($data);
 
                     //autoLogin after register
                     $_SESSION['user'] = $email;
                     $this->returnToOverview('home');
                 } else {
-                    var_dump('Dit is een test'); die();
+                    $this->returnToOverview('register?error=true&email=' . $email);
                 }
             } else {
                 $this->returnToOverview('register?error=true&email=' . $email);
