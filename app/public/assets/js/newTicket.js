f37d636e00210cb59dc17481b46c18b30e99acce
Jonathan De Mangelaere <jonathan@jon-it.be>
2020-12-31 01:13:12 +0100

    Added basic add form (not working)

--- app/public/assets/js/newTicket.js
+++ app/public/assets/js/newTicket.js
@@ -4,6 +4,9 @@
         document.getElementById('existing').click();
         document.getElementById('new').addEventListener('click', newEvent);
         document.getElementById('existing').addEventListener('click', showExisting);
+
+        document.getElementById('ticketDetailsEx').addEventListener('click', showTicketDetailsFormEx);
+        document.getElementById('ticketDetailsNw').addEventListener('click', showTicketDetailsFormNw);
     })
 })();
 
@@ -17,4 +20,16 @@ function newEvent() {
     document.getElementById('newEvent').style.display = "block";
     document.getElementById('existingEvent').style.display = "none";
 
+}
+
+function showTicketDetailsFormEx(e){
+    e.preventDefault();
+    console.log('idfjgghgir');
+    document.getElementById('eventSelection').style.display = "none";
+    document.getElementById('ticketDetails').style.display = "block";
+}
+
+function showTicketDetailsFormNw(e){
+    e.preventDefault();
+    console.log('odfjghrjyhtofjehtrjr');
 }
