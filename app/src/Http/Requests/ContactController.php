7c0f851813b2eb1e29d17df2a64665530276dca8
Moise VanKeymeulen <moise.vankeymeulen@student.odisee.be>
2021-01-05 17:35:50 +0100

    able to invite friends

--- app/src/Http/Requests/ContactController.php
+++ app/src/Http/Requests/ContactController.php
@@ -43,15 +43,15 @@
         private function getName(){
             if (isset($_POST['name']) && $_POST['name'] !== ''){
                 $name = $_POST['name'];
-                $errors['name'] = '';
+                $this->errors['name'] = '';
             }
             elseif (isset($_POST['name']) && $_POST['name'] == ''){
                 $name = '';
-                $errors['name'] = 'Gelieve uw naam in te vullen';
+                $this->errors['name'] = 'Gelieve uw naam in te vullen';
             }
             else{
                 $name = '';
-                $errors['name'] = '';
+                $this->errors['name'] = '';
             }
             return $name;
         }
@@ -59,15 +59,15 @@
         private function getEmail(){
             if (isset($_POST['email']) && $_POST['email'] !== ''){
                 $email = $_POST['email'];
-                $errors['email'] = '';
+                $this->errors['email'] = '';
             }
             elseif (isset($_POST['email']) && $_POST['email'] == ''){
                 $email = '';
-                $errors['email'] = 'Gelieve een mailadres in te vullen';
+                $this->errors['email'] = 'Gelieve een mailadres in te vullen';
             }
             else{
                 $email = '';
-                $errors['email'] = '';
+                $this->errors['email'] = '';
             }
             return $email;
         }
@@ -75,16 +75,16 @@
         private function getSubject(){
             if (isset($_POST['subject']) && $_POST['subject'] !== ''){
                 $subject = $_POST['subject'];
-                $errors['subject'] ='';
+                $this->errors['subject'] ='';
             }
             elseif (isset($_POST['subject']) && $_POST['subject'] == ''){
                 $subject = '';
-                $errors['subject'] = 'Gelieve een onderwerp in te geven';
+                $this->errors['subject'] = 'Gelieve een onderwerp in te geven';
 
             }
             else{
                 $subject = '';
-                $errors['subject'] ='';
+                $this->errors['subject'] ='';
             }
             return $subject;
         }
@@ -92,15 +92,15 @@
         private function getMessage(){
             if (isset($_POST['message']) && $_POST['message'] !== ''){
                 $message = $_POST['message'];
-                $errors['message'] = '';
+                $this->errors['message'] = '';
             }
             elseif (isset($_POST['message']) && $_POST['message'] == ''){
                 $message = '';
-                $errors['message'] = 'Gelieve uw bericht in te vullen';
+                $this->errors['message'] = 'Gelieve uw bericht in te vullen';
             }
             else{
                 $message = '';
-                $errors['message'] = '';
+                $this->errors['message'] = '';
             }
             return $message;
         }
